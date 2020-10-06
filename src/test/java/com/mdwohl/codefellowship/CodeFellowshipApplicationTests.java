81e9f34c97934d16afd27ef59afe34b67bef1991
mdwohl <mdwohl@gmail.com>
2020-10-06 10:10:37 -0700

    initial build from Spring

---
+++ src/test/java/com/mdwohl/codefellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdwohl.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
